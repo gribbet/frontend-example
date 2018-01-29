f891902b8ac484a333901d34612fa7bd201eeea7
Graham <graham.gibbons@gmail.com>
2018-01-28 17:29:55 -0800

    Generic model table

---
+++ source/scripts/component/ModelTable.ts
@@ -0,0 +1,155 @@
+import ClassToggler from "wedges/lib/component/ClassToggler";
+import ClickHandler from "wedges/lib/component/ClickHandler";
+import Container from "wedges/lib/component/Container";
+import Label from "wedges/lib/component/Label";
+import Selector from "wedges/lib/component/Selector";
+import Template from "wedges/lib/component/Template";
+
+import { application } from "..";
+import Model, { idSort, ModelSort } from "../model/Model";
+import ModelService from "../service/ModelService";
+import DataTable, { Column } from "./DataTable";
+import Pagination from "./Pagination";
+
+declare var require: (path: string) => string;
+
+const perPage = 5;
+
+export class PropertyColumn<T> implements Column<T> {
+    constructor(
+        private displayName: string,
+        private getter: (row: T) => string) { }
+    header = new Container([
+        new Label(() => this.displayName),
+        new ClassToggler("header"),
+        new ClassToggler(this.displayName.toLowerCase())
+    ]);
+    cell = (row: T) => new Container([
+        new Label(() => this.getter(row)),
+        new ClassToggler(this.displayName.toLowerCase(), () => true)
+    ]);
+}
+
+export class SortablePropertyColumn<
+    Id,
+    Sort extends ModelSort,
+    Model_ extends Model<Id>> extends PropertyColumn<Model_> {
+
+    constructor(
+        displayName: string,
+        sort: Sort,
+        getter: (model: Model_) => string,
+        private list: () => ModelList<Id, Sort, Model_>
+    ) {
+        super(displayName, getter);
+        this.header = new Container([
+            new ClassToggler("header"),
+            new ClassToggler(displayName.toLowerCase()),
+            new ClassToggler("active", () =>
+                list().sort === sort),
+            new ClassToggler("reverse", () =>
+                list().sort === sort && list().reverse),
+            new Template("<button />"),
+            new Selector("button",
+                new Container([
+                    new Label(() => displayName),
+                    new ClickHandler(() =>
+                        application.update(() => {
+                            const list = this.list();
+                            if (list.sort === sort)
+                                list.reverse = !list.reverse;
+                            if (list.sort !== sort)
+                                list.reverse = false;
+                            list.sort = sort;
+                            list.page = 0;
+                            list.reset();
+                        }))
+                ]))
+        ]);
+    }
+}
+
+class ButtonsColumn<T> implements Column<T> {
+
+    constructor(
+        private onEdit: (row: T) => void,
+        private onDelete: (row: T) => void
+    ) { }
+
+    header = new Container([
+        new ClassToggler("header"),
+        new ClassToggler("buttons")
+    ]);
+
+    cell = (row: T) => new Container([
+        new Template(require("../../templates/buttons-column.pug")),
+        new ClassToggler("buttons", () => true),
+        new Selector(".edit",
+            new ClickHandler(() => this.onEdit(row))),
+        new Selector(".delete",
+            new ClickHandler(() => this.onDelete(row)))
+    ]);
+}
+
+export default class ModelList<
+    Id,
+    Sort extends ModelSort,
+    Model_ extends Model<Id>> extends Container {
+
+    constructor(
+        private service: ModelService<Id, Sort, Model_>,
+        onEdit: (model: Model_) => void,
+        columns: Column<Model_>[],
+    ) {
+        super([
+            new Template(require("../../templates/model-table.pug")),
+            new Selector(".models",
+                new DataTable<Model_>(
+                    () => this.models || [],
+                    columns.concat(
+                        new ButtonsColumn<Model_>(
+                            model => onEdit(model),
+                            async model => {
+                                if (model.id !== null)
+                                    await service.delete(model.id);
+                                await application.update(() => this.reset())
+                            })))),
+            new Selector(".pagination",
+                new Pagination(
+                    () => this.page,
+                    () => Math.ceil((this.count || 0) / perPage),
+                    page =>
+                        application.update(() => {
+                            this.page = page;
+                            this.reset();
+                        })))
+        ]);
+    }
+
+    public page = 0;
+    public sort: Sort = <Sort>idSort;
+    public reverse = false;
+
+    private models: Model_[] | null = null;
+    private count: number | null = null;
+
+    async load() {
+        this.count = this.count
+            || await this.service.count();
+        if (this.page * perPage >= this.count)
+            this.page = Math.floor((this.count - 1) / perPage);
+        this.models = this.models
+            || await this.service.list({
+                count: perPage,
+                offset: this.page * perPage,
+                sort: this.sort,
+                reverse: this.reverse
+            });
+        await super.load();
+    }
+
+    async reset() {
+        this.models = null;
+        this.count = null;
+    }
+}
--- source/scripts/component/WidgetList.ts
+++ source/scripts/component/WidgetList.ts
@@ -1,168 +1,53 @@
-import ClassToggler from "wedges/lib/component/ClassToggler";
 import ClickHandler from "wedges/lib/component/ClickHandler";
 import Container from "wedges/lib/component/Container";
-import Label from "wedges/lib/component/Label";
 import Selector from "wedges/lib/component/Selector";
 import Template from "wedges/lib/component/Template";
 
-import { application, widgetService } from "..";
-import { idSort, ModelSort, updatedSort } from "../model/Model";
-import Widget, { nameSort, WidgetSort } from "../model/Widget";
-import DataTable, { Column } from "./DataTable";
-import Pagination from "./Pagination";
+import { widgetService } from "..";
+import { idSort, updatedSort } from "../model/Model";
+import Widget, { nameSort, WidgetId, WidgetSort } from "../model/Widget";
+import ModelTable, { SortablePropertyColumn } from "./ModelTable";
 
 declare var require: (path: string) => string;
-
-const perPage = 5;
-
-class PropertyColumn<T> implements Column<T> {
-    constructor(
-        private displayName: string,
-        private getter: (row: T) => string) { }
-    header = new Container([
-        new Label(() => this.displayName),
-        new ClassToggler("header"),
-        new ClassToggler(this.displayName.toLowerCase())
-    ]);
-    cell = (row: T) => new Container([
-        new Label(() => this.getter(row)),
-        new ClassToggler(this.displayName.toLowerCase(), () => true)
-    ]);
-}
-
-class SortablePropertyColumn<T, Sort_ extends ModelSort> extends PropertyColumn<T> {
-
-    constructor(
-        displayName: string,
-        sort: WidgetSort,
-        getter: (row: T) => string,
-        private list: () => WidgetList
-    ) {
-        super(displayName, getter);
-        this.header = new Container([
-            new ClassToggler("header"),
-            new ClassToggler(displayName.toLowerCase()),
-            new ClassToggler("active", () =>
-                list().sort === sort),
-            new ClassToggler("reverse", () =>
-                list().sort === sort && list().reverse),
-            new Template("<button></button>"),
-            new Selector("button",
-                new Container([
-                    new Label(() => displayName),
-                    new ClickHandler(() =>
-                        application.update(() => {
-                            const list = this.list();
-                            if (list.sort === sort)
-                                list.reverse = !list.reverse;
-                            if (list.sort !== sort)
-                                list.reverse = false;
-                            list.sort = sort;
-                            list.page = 0;
-                            list.reset();
-                        }))
-                ]))
-        ]);
-    }
-}
-
-
-
-class ButtonsColumn<T> implements Column<T> {
-
-    constructor(
-        private onEdit: (row: T) => void,
-        private onDelete: (row: T) => void
-    ) { }
-
-    header = new Container([
-        new ClassToggler("header"),
-        new ClassToggler("buttons")
-    ]);
-
-    cell = (row: T) => new Container([
-        new Template(require("../../templates/buttons-column.pug")),
-        new ClassToggler("buttons", () => true),
-        new Selector(".edit",
-            new ClickHandler(() => this.onEdit(row))),
-        new Selector(".delete",
-            new ClickHandler(() => this.onDelete(row)))
-    ]);
-}
 
 export default class WidgetList extends Container {
 
     constructor(
         events: {
             onNew: () => void,
-            onEdit: (widget: Widget) => void
-        }
-    ) {
+            onEdit: (model: Widget) => void
+        }) {
+        const table =
+            new ModelTable(widgetService, events.onEdit, [
+                new SortablePropertyColumn<WidgetId, WidgetSort, Widget>(
+                    "ID",
+                    idSort,
+                    _ => (_.id || 0).toString(),
+                    () => this.table),
+                new SortablePropertyColumn<WidgetId, WidgetSort, Widget>(
+                    "Name",
+                    nameSort,
+                    _ => _.name,
+                    () => this.table),
+                new SortablePropertyColumn<WidgetId, WidgetSort, Widget>(
+                    "Updated",
+                    updatedSort,
+                    _ => _.updated.toLocaleTimeString(),
+                    () => this.table)
+            ]);
         super([
             new Template(require("../../templates/widget-list.pug")),
             new Selector(".new",
                 new ClickHandler(() => events.onNew())),
-            new Selector(".widgets",
-                new DataTable<Widget>(
-                    () => this.widgets || [], [
-                        new SortablePropertyColumn<Widget, WidgetSort>(
-                            "ID",
-                            idSort,
-                            widget => (widget.id || 0).toString(),
-                            () => this),
-                        new SortablePropertyColumn<Widget, WidgetSort>(
-                            "Name",
-                            nameSort,
-                            widget => widget.name,
-                            () => this),
-                        new SortablePropertyColumn<Widget, WidgetSort>(
-                            "Updated",
-                            updatedSort,
-                            widget => widget.updated.toLocaleTimeString(),
-                            () => this),
-                        new ButtonsColumn<Widget>(
-                            widget => events.onEdit(widget),
-                            async  widget => {
-                                await widgetService.delete(widget.id);
-                                await application.update(() => this.reset());
-                            })
-                    ])),
-            new Selector(".pagination",
-                new Pagination(
-                    () => this.page,
-                    () => Math.ceil((this.count || 0) / perPage),
-                    page =>
-                        application.update(() => {
-                            this.page = page;
-                            this.reset();
-                        })))
+            new Selector(".model-table", table)
         ]);
+        this.table = table;
     }
 
-    public page = 0;
-    public sort: WidgetSort = idSort;
-    public reverse = false;
+    private table: ModelTable<WidgetId, WidgetSort, Widget>;
 
-    private widgets: Widget[] | null = null;
-    private count: number | null = null;
-
-    async load() {
-        this.count = this.count
-            || await widgetService.count();
-        if (this.page * perPage >= this.count)
-            this.page = Math.floor((this.count - 1) / perPage);
-        this.widgets = this.widgets
-            || await widgetService.list({
-                count: perPage,
-                offset: this.page * perPage,
-                sort: this.sort,
-                reverse: this.reverse
-            });
-        await super.load();
-    }
-
-    async reset() {
-        this.widgets = null;
-        this.count = null;
+    reset() {
+        this.table.reset();
     }
 }
+
