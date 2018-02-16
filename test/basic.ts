f10fec060389c0923e6119873b85e8ac32b2f517
Graham <graham.gibbons@gmail.com>
2018-02-16 08:55:21 -0800

    Add dummy test

---
+++ test/basic.ts
@@ -0,0 +1,11 @@
+import "mocha";
+
+import { expect } from "chai";
+
+describe("Dummy test", () => {
+
+    it("should be able to add things correctly", () => {
+        expect(3 + 4).to.equal(7);
+    });
+
+});
