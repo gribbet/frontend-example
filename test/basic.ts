47b72518c530fd3fde4db9c1d5f0db872b07a9ff
Graham <graham.gibbons@gmail.com>
2018-02-16 12:18:10 -0800

    Add some tests

--- test/basic.ts
+++ test/basic.ts
@@ -1,11 +1,83 @@
 import "mocha";
 
-import { expect } from "chai";
+import { assert } from "chai";
+import * as express from "express";
+import * as http from "http";
+import * as puppeteer from "puppeteer";
+import * as serveStatic from "serve-static";
 
-describe("Dummy test", () => {
+const port = 8080;
+const server: Promise<http.Server> =
+    new Promise(resolve => {
+        const app = express();
+        app.use(
+            serveStatic("build", {
+                "index": "index.html"
+            }));
+        resolve(app.listen(port));
+    });
+const browser: Promise<puppeteer.Browser> =
+    new Promise(async resolve =>
+        resolve(await puppeteer.launch()));
+const page: Promise<puppeteer.Page> =
+    new Promise(async resolve =>
+        resolve(await (await browser).newPage()));
 
-    it("should be able to add things correctly", () => {
-        expect(3 + 4).to.equal(7);
+before(async () => {
+    await server;
+    await browser;
+    await page;
+});
+
+after(async () => {
+    (await browser).close();
+    (await server).close();
+});
+
+beforeEach(async () =>
+    await (await page).goto(`http://localhost:${port}/`));
+
+const delay = (time: number) =>
+    new Promise(_ => setTimeout(_, time));
+
+const load = async () => {
+    await delay(50);
+    await (await page).waitForSelector("body:not(.loading)");
+};
+
+const firstNameInList = async () =>
+    await (await page).evaluate(() => {
+        const element = document.querySelector(".name:not(.header)");
+        return element !== null ? element.textContent : "";
+    });
+
+describe("Example tests", () => {
+
+    it("can create new widget", async () => {
+        const newButton = await (await page).waitForSelector("button.new");
+        await newButton.click();
+        const nameInput = await (await page).waitForSelector(".name input");
+        nameInput.type("Test Widget");
+        const submitButton = await (await page).waitForSelector("button[type=submit]");
+        await submitButton.click();
+    });
+
+    it("can sort widgets", async () => {
+        const idHeaderButton = await (await page).waitForSelector(".id.header button");
+        const name1 = await firstNameInList();
+        await idHeaderButton.click();
+        await load();
+        const name2 = await firstNameInList();
+        assert(name1 !== name2, "order changed");
+    });
+
+    it("can delete widget", async () => {
+        const name1 = await firstNameInList();
+        const deleteButton = await (await page).waitForSelector("button.delete");
+        await deleteButton.click();
+        await load();
+        const name2 = await firstNameInList();
+        assert(name1 !== name2, "widget removed");
     });
 
 });
